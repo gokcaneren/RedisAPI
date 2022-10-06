c3d195ed30481da7d0d7c2767e4d5a64e6238ad3
gokcaneren <gokcann01@gmail.com>
2022-10-06 16:02:15 +0300

    Get and Post methods are completed

---
+++ Controllers/PersonController.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Mvc;
+using RedisAPI.Data;
+using RedisAPI.Models;
+
+namespace RedisAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+        private readonly IPersonRepository _repository;
+
+        public PersonController(IPersonRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet("{id}", Name="GetPersonById")]
+        public ActionResult<PersonName> GetPersonById(string id)
+        {
+            var person = _repository.GetPersonById(id);
+
+            if (person != null)
+            {
+                return Ok(person);
+            }
+            return NotFound();
+        }
+
+        [HttpPost]
+        public ActionResult<PersonName> CreatePerson(PersonName personName)
+        {
+            _repository.CreatePerson(personName);
+
+            return CreatedAtRoute(nameof(GetPersonById), new {Id=personName.Id}, personName);
+        }
+    }
+}
